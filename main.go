0ff4e143ff70959fbe50630814a7839f568cd877
takahiroyoshikawa <aihasayahukoma7@gmail.com>
2019-03-03 00:08:01 +0900

    add: main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	director := func(request *http.Request) {
+		request.URL.Scheme = "http"
+		request.URL.Host = ":9000"
+	}
+
+	rp := &httputil.ReverseProxy{
+		Director: director,
+	}
+
+	server := http.Server{
+		Addr:    ":8484",
+		Handler: rp,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
